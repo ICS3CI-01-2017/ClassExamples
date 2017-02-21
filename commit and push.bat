5177543897b4d1cf6b07fa46698bb3353e59260e
Adam Lamont <Adam Lamont>
2017-02-21 11:26:25 -0500

    Added beginning Pong example

--- commit and push.bat
+++ commit and push.bat
@@ -1,5 +1,5 @@
 @echo off
 git add *
-set /p message=Enter the commit message for this git commit
-git commit -m "%message"
+set /p message="Enter the commit message for this git commit: "
+git commit -m "%message%"
 pause
